2e0868a9c3ddfdadb6f0217b66aec4bd716671ed
Dovydas Labutis <dovydaslab@gmail.com>
2020-01-28 15:03:05 +0200

    googlemaps Added

--- app/containers/MapPage/index.js
+++ app/containers/MapPage/index.js
@@ -1,11 +1,28 @@
 import React from 'react';
 import Navbar from 'components/Header/NavBar';
+import { GoogleMap, withScriptjs, withGoogleMap } from 'react-google-maps';
+
+function Map() {
+  return (
+    <GoogleMap
+      defaultZoom={7}
+      defaultCenter={{ lat: 55.146115, lng: 23.79204 }}
+    />
+  );
+}
+
+const WrapperdMap = withScriptjs(withGoogleMap(Map));
 
 export default function ListPage() {
   return (
-    <div>
+    <div style={{ width: '100vw', height: '100vh' }}>
       <Navbar />
-      <p> Map showing Lithuania map with locations</p>
+      <WrapperdMap
+        googleMapURL="https://maps.googleapis.com/maps/api/js?key=AIzaSyCBWjadzeVNNDCooZWv31v41ycKecfKVGU&callback=Map"
+        loadingElement={<div style={{ height: `100%` }} />}
+        containerElement={<div style={{ height: `100%` }} />}
+        mapElement={<div style={{ height: `100%` }} />}
+      />
     </div>
   );
 }
